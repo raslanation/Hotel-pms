381e8da070cd589de54e5f41b7c9b4be02453616
raslanation <ahmed.raslan@gmail.com>
2026-07-19 05:28:22 +0300

    Add database configuration using Sequelize

---
+++ backend/src/config/db.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+    logging: false,
+    define: {
+      // Sequelize default is camelCase in JS <-> camelCase columns
+      underscored: false,
+    },
+  }
+);
+
+module.exports = sequelize;
